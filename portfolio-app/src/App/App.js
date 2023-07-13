d9f48bd909f2cd6deb11516b6bfd4705d35a212b
MateenSQ <111274354+MateenSQ@users.noreply.github.com>
2023-07-13 20:24:29 +0100

    react app

---
+++ portfolio-app/src/App/App.js
@@ -0,0 +1,18 @@
+import logo from '../logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>This will soon be my portfolio page. It will look nice, <strong>VERY</strong> nice</h1>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          YE OLDE REACT
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
