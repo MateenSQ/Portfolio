5428d5e4005ef0c4e54b02d033bef7326564e494
MateenSQ <111274354+MateenSQ@users.noreply.github.com>
2023-07-14 21:11:44 +0100

    added general fade-ins

--- portfolio-app/src/App/App.js
+++ portfolio-app/src/App/App.js
@@ -1,15 +1,43 @@
-import { useState } from 'react'
-import logo from '../logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+function Fade() {
 
+  const [showTitle, setShowTitle] = useState(true);
+  const [showIntro, setShowIntro] = useState(true);
+  const [showPage, setShowPage] = useState(true)
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setShowTitle(false);
+    }, 1000); // Set the timeout duration in milliseconds (e.g., 2000ms = 2 seconds)
+
+    return () => clearTimeout(timeout);
+  }, []);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setShowIntro(false);
+    }, 2500); // Set the timeout duration in milliseconds (e.g., 2000ms = 2 seconds)
+
+    return () => clearTimeout(timeout);
+  }, []);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setShowPage(false);
+    }, 4000); // Set the timeout duration in milliseconds (e.g., 2000ms = 2 seconds)
+
+    return () => clearTimeout(timeout);
+  }, []);
 
   return (
-    <div className="App">
-      <p id="sup">Sup homie</p>
+    <div id='Home'>
+      <h1 className={`Fade ${showTitle ? 'fade-in' : 'fade-out'}`} id="sup">Welcome</h1>
+      <h2 className={`Fade ${showIntro ? 'fade-in' : 'fade-out'}`}>I'm Mateen, a junior software developer</h2>
+      <p className={`Fade ${showPage ? 'fade-in' : 'fade-out'}`}>This is my page</p>
     </div>
   );
 }
 
-export default App;
+export default Fade;
