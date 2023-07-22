5e21f397d2b57d0deb18965f70863b703a0d65b8
MateenSQ <111274354+MateenSQ@users.noreply.github.com>
2023-07-22 18:23:50 +0100

    got fundamental positioning underway

--- portfolio-app/src/Card/Card.js
+++ portfolio-app/src/Card/Card.js
@@ -3,10 +3,12 @@ import img from '../images/gardenGenieLogo.PNG'
 
 function Card() {
     return (
-        <div className="card">  
-            <a className="flexItem" href="https://garden-genie.netlify.app/" target="_blank"rel="noreferrer" >
-                <header className="cardHeader" >Garden Genie</header>
-                <p className="cardText">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tortor at risus viverra adipiscing. Amet volutpat consequat mauris nunc congue nisi vitae. Eu sem integer vitae justo eget magna fermentum iaculis. Tempus quam pellentesque nec nam aliquam sem et tortor. Sed vulputate mi sit amet. Mi bibendum neque egestas congue quisque egestas diam in.</p>
+        <div>  
+            <a className="card" href="https://garden-genie.netlify.app/" target="_blank"rel="noreferrer">
+                <header className="cardHeader">Garden Genie</header>
+                <div className="cardText">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tortor at risus viverra adipiscing. Amet volutpat consequat mauris nunc congue nisi vitae. Eu sem integer vitae justo eget magna fermentum iaculis. Tempus quam pellentesque nec nam aliquam sem et tortor. Sed vulputate mi sit amet. Mi bibendum neque egestas congue quisque egestas diam in.</p>
+                </div>
                 <img className="cardImage" src={img} alt="Garden Genie Logo" />
             </a>
         </div>
