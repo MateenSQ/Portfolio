22e41ac7ffa5a7bd694b18e680210610c96c7ce0
MateenSQ <111274354+MateenSQ@users.noreply.github.com>
2023-07-24 19:33:36 +0100

    finally got it on the same line as the image

--- portfolio-app/src/Card/Card.js
+++ portfolio-app/src/Card/Card.js
@@ -6,9 +6,11 @@ function Card({aHref, header, desc, imgSrc, imgAlt}) {
         <div className="card">  
             <header className="cardHeader">{header}</header>
         <div className="text-ImgBox">
-            <p className="cardText">{desc}</p>
-            <a className="" href={aHref} target="_blank"rel="noreferrer">
-                <img className="cardImage" src={imgSrc} alt={imgAlt} />
+            <div className="cardText">
+                <p>{desc}</p>
+            </div>
+            <a className="cardImages" href={aHref} target="_blank"rel="noreferrer">
+                <img src={imgSrc} alt={imgAlt} />
             </a>
             </div>
         </div>
