eeb7f76e3b4d0e48a869275ed644bf6cf5b0c3c3
MateenSQ <111274354+MateenSQ@users.noreply.github.com>
2023-08-15 23:41:52 +0100

    added bug awareness

--- portfolio-app/src/Main/Main.js
+++ portfolio-app/src/Main/Main.js
@@ -4,12 +4,12 @@ import GardenGenieImg from '../images/gardenGenieLogo.PNG'
 import HandbookImg from '../images/handbookLogo.PNG'
 function Main({showPage}) {
 
-    const gardenGenieDesc = "Garden Genie by the team M.A.S.H.E.D. This was our final, 5 week long project at the School of Code. Using everything we've learnt during the boot camp AND more, we put it all in a project again - in the School of Code way - in random teams! Find a more detailed description and view things we did along the project like our FigJam and prototype on the GitHub repository's README.md"
-    const onlineHandbookDesc = "Online Student Handbook - our first ever project at the School of Code. Given only a week, we were tasked with creating an app that solves a problem faced by fellow students. Given only a week to gather ideas, plan, create and present our app, it was great learning experience. Find out more about the process on the GitHub repository's README.md"
+    const gardenGenieDesc = "Garden Genie by the team M.A.S.H.E.D. This was our final, 5 week long project at the School of Code. Using everything we've learnt during the boot camp AND more, we put it all in a project again - in the School of Code way - in random teams! Find a more detailed description and view things we did along the project like our FigJam and prototype on the GitHub repository's README.md. WARNING - there is an ocassional bug at the form page. If the submit button freezes the page, close the tab and open the app again."
+    const onlineHandbookDesc = "Online Student Handbook - our first ever project at the School of Code. Given only a week, we were tasked with creating an app that solves a problem faced by fellow students. Given only a week to gather ideas, plan, create and present our app, it was great learning experience. Find out more about the process on the GitHub repository's README.md."
 
     return (
         <main className={`text fade ${showPage ? 'fade-in' : 'fade-out'}`}>
-                <h3 className="h3">Here you will find projects I have worked on - click on the image to go to the website! This is the first version of my portfolio site, expect changes to come!</h3>
+                <h3 className="h3">Here you will find projects I have worked on - click on the images to go to the website! This is the first version of my portfolio site, expect changes to come!</h3>
 
                  <Card 
                     aHref="https://garden-genie.netlify.app/"
